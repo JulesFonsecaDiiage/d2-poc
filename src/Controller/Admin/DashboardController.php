a1d8108c70bdc6ea813b6d5d5bf87f5a8b6a323c
Jules Fonseca <jules.fonseca@groupe8.diiage.org>
2025-06-15 10:49:55 +0200

    Improve dashboard

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -4,10 +4,14 @@ namespace App\Controller\Admin;
 
 use App\DTO\EntiteDto;
 use App\Entity\Default\Entite;
+use App\Entity\Default\Expedition;
 use App\Entity\Default\Produit;
 use App\Entity\Facturation\PrestationDiversConsolide;
 use App\Entity\Facturation\PrestationDiversConsolidePrestation;
 use App\Repository\Default\EntiteRepository;
+use App\Repository\Default\ExpeditionRepository;
+use DateTimeImmutable;
+use Doctrine\Common\Collections\Criteria;
 use EasyCorp\Bundle\EasyAdminBundle\Attribute\AdminDashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Action;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Actions;
@@ -20,16 +24,50 @@ use Symfony\Component\HttpFoundation\Response;
 #[AdminDashboard(routePath: '/admin', routeName: 'admin')]
 class DashboardController extends AbstractDashboardController
 {
-    public function __construct(private EntiteRepository $entiteRepository) {}
+    public function __construct(
+        private readonly EntiteRepository $entiteRepository,
+        private readonly ExpeditionRepository $expeditionRepository,
+    ) {}
 
     public function index(): Response
     {
         $users = $this->entiteRepository->findBy([], ['created_at' => 'DESC'], 10);
         $nbActiveUsers = $this->entiteRepository->count(['active' => true]);
 
+        $criteria = Criteria::create()
+            ->andWhere(Criteria::expr()->gte('created_at', new DateTimeImmutable('-1 month')))
+            ->orderBy(['created_at' => 'DESC']);
+
+        $expeditionsDernierMois = $this->expeditionRepository->matching($criteria);
+        $nbExpeditions = $expeditionsDernierMois->count();
+
+        $revenues = $expeditionsDernierMois->reduce(fn(float $total, Expedition $expedition) => $total + $expedition->getTotal(), 0.0);
+        $revenues = number_format($revenues, 0, ',', ' ') . ' €';
+
+        $chartData = [];
+        $criteria = Criteria::create()
+            ->andWhere(Criteria::expr()->gte('created_at', new \DateTimeImmutable('-1 year')))
+            ->orderBy(['created_at' => 'ASC']);
+
+        $formatter = new \IntlDateFormatter('fr_FR', pattern: 'LLLL yyyy');
+        $expeditionsDerniereAnnee = $this->expeditionRepository->matching($criteria);
+
+        foreach ($expeditionsDerniereAnnee as $expedition) {
+            $date = $formatter->format($expedition->getCreatedAt());
+            $chartData[$date] = ($chartData[$date] ?? 0) + $expedition->getTotal();
+        }
+
+        $chartLabels = array_keys($chartData);
+        $chartValues = array_values($chartData);
+
         return $this->render('admin/dashboard.html.twig', [
             'users' => $users,
             'nbActiveUsers' => $nbActiveUsers,
+            'expeditions' => $expeditionsDernierMois,
+            'nbExpeditions' => $nbExpeditions,
+            'revenues' => $revenues,
+            'chartLabels' => json_encode($chartLabels),
+            'chartValues' => json_encode($chartValues),
         ]);
     }
 
@@ -46,6 +84,10 @@ class DashboardController extends AbstractDashboardController
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
 
+        yield MenuItem::section('Ventes');
+        yield MenuItem::linkToCrud('Produits', 'fas fa-box', Produit::class);
+        yield MenuItem::linkToCrud('Expeditions', 'fas fa-shipping-fast', Expedition::class);
+
         yield MenuItem::section('CRM');
         yield MenuItem::linkToCrud('Entité', 'fas fa-users', Entite::class);
         yield MenuItem::linkToCrud('Entité (custom)', 'fas fa-users', EntiteDto::class);
@@ -55,8 +97,6 @@ class DashboardController extends AbstractDashboardController
             MenuItem::linkToCrud('Divers (Consolidations)', null, PrestationDiversConsolide::class),
             MenuItem::linkToCrud('Divers (Prestations)', null, PrestationDiversConsolidePrestation::class),
         ]);
-
-        yield MenuItem::linkToCrud('Produits', 'fas fa-box', Produit::class);
     }
 
     public function configureActions(): Actions
